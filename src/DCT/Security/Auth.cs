a45fa56d25531250181a72b9573de4b77641ec11
typpo <typppo@gmail.com>
2009-12-25 17:49:01 +0000

    deleted auth

--- src/DCT/Security/Auth.cs
+++
@@ -1,9 +0,0 @@
-namespace DCT.Security
-{
-    static internal class Auth
-    {
-        //internal const string CHECK =
-        //    "5bc1b6c967299387254fefae3b7dd5e7444ee72b8024243b5bd7ed6a4a8db40daf30f53fb8ea010e12dca8ae";
-        internal const string KEY = "Ug4@j-)\\ ";
-    }
-}
