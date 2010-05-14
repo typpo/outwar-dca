0a3f4c18d8cb25a9cb3b90e097792067e86f5447
typpo <typppo@gmail.com>
2010-05-14 01:16:53 +0000

    added MobCollector, prevented duplicate mob submissions

---
+++ src/DCT/Pathfinding/MobCollector.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using DCT.Outwar.World;
+using DCT.Protocols.Http;
+
+namespace DCT.Pathfinding
+{
+    /// <summary>
+    /// Static class that keeps track of mobs collected and submits them for inclusion in the mob list.
+    /// </summary>
+    internal static class MobCollector
+    {
+        private const string SUBMIT_URL = "http://typpo.us/submit_mobs.php";
+
+        private static List<MappedMob> mMobs;
+        private static List<MappedMob> mSubmitted;
+
+        internal static int Count
+        {
+            get { return mMobs.Count; }
+        }
+
+        static MobCollector()
+        {
+            mMobs = new List<MappedMob>();
+        }
+
+        internal static void Add(Mob mb)
+        {
+            MappedMob mm = new MappedMob(mb.Name, mb.Id, mb.Room.Id, mb.Level, mb.Rage);
+            if (!mMobs.Contains(mm) && !mSubmitted.Contains(mm))
+            {
+                mMobs.Add(mm);
+            }
+        }
+
+        internal static void Submit()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (MappedMob mb in mMobs)
+            {
+                sb.AppendLine(mb.ToString());
+            }
+
+            // send data
+            HttpSocket s = new HttpSocket();
+            s.Post(SUBMIT_URL, string.Format("mobs={0}", HttpUtility.UrlEncode(sb.ToString())));
+
+            mSubmitted.AddRange(mMobs);
+            mMobs.Clear();
+        }
+    }
+}
