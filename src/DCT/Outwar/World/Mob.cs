97de84e315392a8204237d81393afaee2bc00724
typpo <typppo@gmail.com>
2010-08-01 19:51:00 +0000

    fixed drop bug http://typpo.us/trac/repos/ticket/77

--- src/DCT/Outwar/World/Mob.cs
+++ src/DCT/Outwar/World/Mob.cs
@@ -491,7 +491,7 @@ namespace DCT.Outwar.World
                     //}
                     //else
                     //{
-                        string[] fs = Parser.MultiParse(src.ToLower(), string.Format("{0} found ", mRoom.Mover.Account.Name).ToLower(), "!<br>");
+                        string[] fs = Parser.MultiParse(src, "Found ", "</b>");
                         if (fs.Length > 1)
                         {
                             bool reported = false;  // flag to keep track of whether this bug has been reported
